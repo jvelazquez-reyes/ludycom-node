b8f7547d4f53d4c6550afa935ac42f96bf9fd5b0
Juan <velazquezrj2012@licifug.ugto.mx>
2023-05-26 14:07:32 -0600

    Update the Dockerfiles and docker-compse

--- setup.sql
+++ setup.sql
@@ -1,21 +1,31 @@
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `names` VARCHAR(50),
-    `surnames` VARCHAR(50),
-    `birthdate` DATE,
-    `email` VARCHAR(50),
-    `document` INT(7),
-    `code` INT(2),
-    `salary` DECIMAL(10,2),
-    `status` BOOLEAN DEFAULT TRUE,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS user (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) DEFAULT NULL,
+    password VARCHAR(10) DEFAULT NULL,
+    PRIMARY KEY  (id)
+);
 
-CREATE TABLE IF NOT EXISTS `workplace` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `code` INT(2),
-    `name` VARCHAR(50),
-    `manager` INT(7),
-    `status` BOOLEAN DEFAULT TRUE,
-    `PRIMARY` KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+INSERT INTO user values
+    (1, 'Ludycom', 'test');
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    names VARCHAR(50),
+    surnames VARCHAR(50),
+    birthdate DATE,
+    email VARCHAR(50),
+    document INT(7),
+    code INT(2),
+    salary DECIMAL(10,2),
+    status BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS workplace (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    code INT(2),
+    name VARCHAR(50),
+    manager INT(7),
+    status BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY (id)
+);
